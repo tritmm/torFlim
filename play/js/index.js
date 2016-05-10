7111a5b2f7dabb2c90b5628bd2ded40911e19ad8
Huỳnh Quang Trang <quangtrang1111>
2016-05-10 22:36:29 +0700

    - video player

--- play/js/index.js
+++ play/js/index.js
@@ -1,31 +1,97 @@
-var player, video
-
-window.onload = function () {
-  player = document.getElementById('player');
-  video = document.getElementById('video-player');
-}
-
 blocks.query(new Player())
 
 function Player() {
-  this.isPlaying = false
-  this.playClass = blocks.observable('glyphicon-play')
+  this.isPlaying = blocks.observable(false)
+  this.isFullScreen = blocks.observable(false)
+  this.isShowControl = blocks.observable(true)
+  this.playTime = blocks.observable(0)
+  this.videoDuration = blocks.observable(0)
+  this.subTop = blocks.observable('')
+  this.subBottom = blocks.observable('')
 
-  this.isFullScreen = false;
-  this.fullScreenClass = blocks.observable('glyphicon-resize-full')
+  this.duration = blocks.observable(function () {
+    return formatTime(this.videoDuration())
+  }, this)
+  this.currentTime = blocks.observable(function () {
+    return formatTime(this.playTime())
+  }, this)
+
+  this.video
+  this.subtitlesTop
+  this.subcountTop = 0
+  this.subtitlesBottom
+  this.subcountBottom = 0
+
+  this.updateSub = function (subtitles) {
+    var subtitle = ''
+
+    // check if the next subtitle is in the current time range
+    if (this.video.currentTime.toFixed(1) > videosub_timecode_min(subtitles[this.subcountTop][1])
+          && this.video.currentTime.toFixed(1) < videosub_timecode_max(subtitles[this.subcountTop][1])) {
+      subtitle = subtitles[this.subcountTop][2]
+    }
+
+    // is there a next timecode?
+    if (this.video.currentTime.toFixed(1) > videosub_timecode_max(subtitles[this.subcountTop][1])
+          && this.subcountTop < (subtitles.length - 1)) {
+      this.subcountTop++
+    }
+
+    return subtitle
+  }
+
+  this.seekSub = function (subtitles) {
+    this.subcountTop = 0
+
+    while (videosub_timecode_max(subtitles[this.subcountTop][1]) < this.video.currentTime.toFixed(1)) {
+			this.subcountTop++
+			if (this.subcountTop > subtitles.length - 1) {
+        this.subcountTop = subtitles.length - 1
+        break;
+			}
+    }
+  }
+
+  this.videoHandle = function (event) {
+    switch (event.type) {
+      case 'loadeddata':
+        this.video = event.target
+        this.videoDuration(this.video.duration)
+
+        var that = this
+        getData('media/WhatCouldIDo-sub-en.srt', function (data) {
+          that.subtitlesTop = parseSub(data)
+        })
+        getData('media/WhatCouldIDo-sub-vi.srt', function (data) {
+          that.subtitlesBottom = parseSub(data)
+        })
+        break;
+      case 'timeupdate':
+        this.playTime(this.video.currentTime)
+
+        this.subTop(this.updateSub(this.subtitlesTop))
+        this.subBottom(this.updateSub(this.subtitlesBottom))
+        break
+      case 'seeked':
+        this.seekSub(this.subtitlesTop)
+        this.seekSub(this.subtitlesBottom)
+        break
+      case 'ended':
+        this.isPlaying(false)
+        break
+      case 'click':
+        this.isPlaying() ? this.video.pause() : this.video.play()
+        this.isPlaying(!this.isPlaying())
+        break        
+    }
+  }
 
   this.clickControl = function (event) {
     switch (event.target.id) {
       case 'play':
-        if (this.isPlaying) {
-          video.pause()
-          this.playClass('glyphicon-play')
-        } else {
-          video.play()
-          this.playClass('glyphicon-pause')
-        }
-
-        this.isPlaying = !this.isPlaying
+        this.isPlaying() ? this.video.pause() : this.video.play()
+        this.isPlaying(!this.isPlaying())
+        this.hideControl()
         break
 
       case 'sound':
@@ -37,26 +103,60 @@ function Player() {
       case 'setting':
         break
       case 'full':
-        if (this.isFullScreen) {
-          requestFullScreen(player, true)
-          this.fullScreenClass('glyphicon-resize-full')
-        } else {
-          requestFullScreen(player)
-          this.fullScreenClass('glyphicon-resize-small')
-        }
-
-        this.isFullScreen = !this.isFullScreen
+        this.isFullScreen() ? changeFullScreenState(player, true) : changeFullScreenState(player)
+        this.isFullScreen(!this.isFullScreen())
         break
     }
   }
+  
+  this.mouseMove = function (event) {
+    this.isShowControl(true)
+
+    if (this.isPlaying()) {
+      this.hideControl()
+    }
+  }
+    
+  this.clickProgress = function (event) {
+    var padding = parseFloat(window.getComputedStyle(event.target).paddingLeft)
+    this.video.currentTime = ((event.offsetX - padding) / (event.target.offsetWidth - padding * 2)) * event.target.max
+  }
+  
+  this.hideControl = function () {
+    var that = this
+    var lastTimeMouseMoved = new Date().getTime();
+    setTimeout(function () {
+      var currentTime = new Date().getTime();
+      if (currentTime - lastTimeMouseMoved >= 5000) {
+        that.isShowControl(false)
+      }
+    }, 5000)
+  }
 }
 
-function requestFullScreen(element, isExit) {
+function formatTime(seconds) {
+  // multiply by 1000 because Date() requires miliseconds
+  var date = new Date(seconds * 1000)
+  var hh = date.getUTCHours()
+  var mm = date.getUTCMinutes()
+  var ss = date.getSeconds()
+
+  // If you were building a timestamp instead of a duration, you would uncomment the following line to get 12-hour (not 24) time
+  // if (hh > 12) {hh = hh % 12;}
+  // These lines ensure you have two-digits
+  if (hh < 10) { hh = '0' + hh }
+  if (mm < 10) { mm = '0' + mm }
+  if (ss < 10) { ss = '0' + ss }
+
+  // This formats your string to HH:MM:SS
+  return (hh == '00') ? mm + ':' + ss : hh + ':' + mm + ':' + ss
+}
+
+function changeFullScreenState(element, isExit) {
   var requestMethod
-  
+
   if (isExit) {
     requestMethod = document.cancelFullScreen || document.webkitCancelFullScreen || document.mozCancelFullScreen || document.msCancelFullScreen
-    console.log(requestMethod)
     requestMethod.call(document);
     return
   }
@@ -72,4 +172,48 @@ function requestFullScreen(element, isExit) {
       wscript.SendKeys("{F11}");
     }
   }
+}
+
+function videosub_timecode_min(tc) {
+		tcpair = tc.split('-->');
+		return videosub_tcsecs(tcpair[0]);
+}
+
+function videosub_timecode_max(tc) {
+		tcpair = tc.split('-->');
+		return videosub_tcsecs(tcpair[1]);
+}
+
+function videosub_tcsecs(tc) {
+		tc1 = tc.split(',');
+		tc2 = tc1[0].split(':');
+		secs = Math.floor(tc2[0] * 60 * 60) + Math.floor(tc2[1] * 60) + Math.floor(tc2[2]);
+		return secs;
+}
+
+function getData(url, callback) {
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', encodeURI(url))
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      callback(xhr.responseText)
+    }
+    else {
+    }
+  }
+
+  xhr.send()
+}
+
+function parseSub(sub) {
+  subtitles = new Array()
+  records = sub.split('\n\n')
+
+  for (var r = 0; r < records.length; r++) {
+    record = records[r]
+    subtitles[r] = new Array()
+    subtitles[r] = record.split('\n')
+  }
+
+  return subtitles
 }
